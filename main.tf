ec9df5751c0a6ca4315b441ad1dc9a0a2c1d78f9
gocd <bst_dcita_gmdo@santander.pt>
2020-08-07 12:29:34 +0200

    added encrypt to S3

--- main.tf
+++ main.tf
@@ -16,5 +16,6 @@ terraform {
     bucket = "pmrm-us-bucket-terraform" # Bucket regiao us east
     key    = "terraform-test.tfstate"
     region = "us-east-1"
+    encrypt = true #encriptar dados sensiveis in S3
   }
 }
